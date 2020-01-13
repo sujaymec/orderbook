10b4f7817e522cd88e3d1d4c0d5805752949baf3
sujaymec <sujay.mec@gmail.com>
2020-01-13 21:08:42 +0530

    Updates

---
+++ precache-manifest.0e1d30a70db44dfb65da4542e6fb6377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9f47a87c140c80eb3bc91db4a1d4eb",
+    "url": "/orderbook/index.html"
+  },
+  {
+    "revision": "2ab4a58e0720176391b5",
+    "url": "/orderbook/static/css/main.24a668a4.chunk.css"
+  },
+  {
+    "revision": "5c100063da36964cf23f",
+    "url": "/orderbook/static/js/2.b4394486.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/orderbook/static/js/2.b4394486.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ab4a58e0720176391b5",
+    "url": "/orderbook/static/js/main.2fc7faaf.chunk.js"
+  },
+  {
+    "revision": "9a1d295f962f095c7c49",
+    "url": "/orderbook/static/js/runtime-main.9ffdcfed.js"
+  }
+]);
