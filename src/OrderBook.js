3b2a1ccf81001f215f8c4044170746acbe6beb03
sujaymec <sujay.mec@gmail.com>
2020-01-13 18:42:01 +0530

    adding color scheme

--- src/OrderBook.js
+++ src/OrderBook.js
@@ -3,20 +3,32 @@ import React from 'react'
 export default function OrderBook(props){
     console.log(props.orderBook.data)
     let buy_book_list = props.orderBook.data.buy_book.map((value, index) => 
-        (<li key={index}>{value.price} {index} </li>)
+        (
+            <li key={value.price}>
+                <span className="order-data-cell text-left">{value.price}</span>
+                <span className="order-data-cell alt-color text-center">{value.size}</span>
+                <span className="order-data-cell alt-color text-right">1000000</span>
+            </li>
+        )
     )
     let sell_book_list = props.orderBook.data.sell_book.map((value, index) => 
-        (<li key={index}>{value.price}</li>)
+        (
+            <li key={value.price}>
+                <span className="order-data-cell text-left">{value.price}</span>
+                <span className="order-data-cell alt-color text-center">{value.size}</span>
+                <span className="order-data-cell alt-color text-right">1000000</span>
+            </li>
+        )
     )
     return(
         <div>
             <h1>Order Book</h1>
             <div id="order-lists">
-                <ul id="buy-book" className="book-list">{buy_book_list}</ul> 
+                <ul id="sell-book" className="book-list">{sell_book_list}</ul>
                 <div id="last-traded" >
                 &#36;{props.orderBook.data.last_trade.price}
                 </div>
-                <ul id="sell-book" className="book-list">{sell_book_list}</ul>                 
+                <ul id="buy-book" className="book-list">{buy_book_list}</ul>                  
             </div> 
 
         </div>
