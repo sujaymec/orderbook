a381a482d6ddd8586c4d9958113195a5be2fe27c
sujaymec <sujay.mec@gmail.com>
2020-01-13 15:38:30 +0530

    css changes

--- src/OrderBook.js
+++ src/OrderBook.js
@@ -10,12 +10,15 @@ export default function OrderBook(props){
     )
     return(
         <div>
-            <h1>Order Book</h1> 
-            <ul id="buy-book" className="book-list">{buy_book_list}</ul> 
-            <div id="last-traded" >
-                {props.orderBook.data.last_trade.price}
-            </div>
-            <ul id="sell-book" className="book-list">{sell_book_list}</ul> 
+            <h1>Order Book</h1>
+            <div id="order-lists">
+                <ul id="buy-book" className="book-list">{buy_book_list}</ul> 
+                <div id="last-traded" >
+                &#36;{props.orderBook.data.last_trade.price}
+                </div>
+                <ul id="sell-book" className="book-list">{sell_book_list}</ul>                 
+            </div> 
+
         </div>
     )
 }
